36c338b4b91232cd76e02a051e4f57b9637b9e9a
Phoenixdadhev <ayomikuntemitope.araoye@gmail.com>
2026-07-11 19:37:45 -0700

    udpated workspace packages name

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono, Public_Sans, IBM_Plex_Sans } from "next/font/google"
 
-import "@workspace/ui/globals.css"
+import "@payweave/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@workspace/ui/lib/utils";
+import { cn } from "@payweave/ui/lib/utils";
 
 const ibmPlexSansHeading = IBM_Plex_Sans({subsets:['latin'],variable:'--font-heading'});
 
--- packages/eslint-config/README.md
+++ packages/eslint-config/README.md
@@ -1,3 +1,3 @@
-# `@workspace/eslint-config`
+# `@payweave/eslint-config`
 
 Shared eslint configuration for the workspace.
